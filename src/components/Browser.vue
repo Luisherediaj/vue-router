0dcac0f11bab5289690307e71292cb4a12fe18c6
Luis <luis.here.j@gmail.com>
2021-03-09 09:57:37 +0100

    Integracion a git del proyecto

---
+++ src/components/Browser.vue
@@ -0,0 +1,14 @@
+<template>
+  <div> Buscador lateral</div>
+</template>
+
+<script>
+export default {
+    Name: 'Browser'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Contenido</div>
+</template>
+
+<script>
+export default {
+    Name: 'Main'
+
+}
+</script>
+
+<style>
+
+</style>
